548f1d65e05a7b12e4c632ec1d03c69a84dfb55e
George Beridze <georgeberidze@Georges-iMac.lan>
2022-04-03 18:12:53 -0700

    Computer can now find winning moves. If not possible, it will defend a critical cell

--- board.py
+++ board.py
@@ -1,26 +1,20 @@
 # Creates a new board, keeps track of score and validates moves
 
 class Board:
-    def __init__(self, new):
-        self.new_game = new
-    
-    cells = {1:" ", 2:" ", 3:" ", 4:" ", 5:" ", 6:" ", 7:" ", 8:" ",9:" "}
+    def __init__(self):
+        self.cells = {1:" ", 2:" ", 3:" ", 4:" ", 5:" ", 6:" ", 7:" ", 8:" ",9:" "}
+        self.symbols = ("X", "0")
 
-    @classmethod
-    def show_board(cls):
-        for key, value in cls.cells.items():
+    def show_board(self):
+        for key, value in self.cells.items():
             row = ""
             row += f"[{value}]"
             if key % 3  == 0:
                 row += "\n"
             print(row, end="")
 
-    @classmethod
-    def make_move(cls, cell, symbol):
-        if cell in cls.cells.keys() and cls.cells[cell] == " ":
-            cls.cells[cell] = symbol
+    def make_move(self, cell, controller):
+        if cell in self.cells.keys() and self.cells[cell] == " ":
+            self.cells[cell] = controller.symbol
             return
         print("You can't move to that cell!")
-
-b1 = Board(True)
-b1.show_board()
